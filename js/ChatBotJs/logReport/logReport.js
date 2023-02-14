a5484902b4f06b2f651675153ecc1327cbad8934
dhanrajpa <dhanrajpa@cybage.com>
2023-02-14 09:13:26 +0530

    log report array added

---
+++ js/ChatBotJs/logReport/logReport.js
@@ -0,0 +1,15 @@
+/**
+ *   "start-time": "x",
+      "end-time": "y",
+      "empId": "27052",
+      "catName": "xyz",
+      "questionInteraction": "xyz?",
+      "QuestionAnswer": ".xyz",
+      "id": 1
+ * 
+ */
+
+
+
+const sessionStartDate = new Date();
+console.log(date);     
